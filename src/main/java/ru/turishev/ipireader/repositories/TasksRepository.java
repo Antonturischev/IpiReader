10b11af6f29dacb63a5aa29fbb44addf469df321
antonturischev <t-antoha@mail.ru>
2020-03-03 22:45:21 +0700

    исправлен расширенный поиск

--- src/main/java/ru/turishev/ipireader/repositories/TasksRepository.java
+++ src/main/java/ru/turishev/ipireader/repositories/TasksRepository.java
@@ -28,45 +28,59 @@ public interface TasksRepository extends JpaRepository<Task,Long> {
     Page<Task> findTasks4MeAndSubordinates(@Param("users") List<User> users, @Param("groups") List<Group> groups, Pageable pageable);
 
     @Query(value = "select distinct (t.*) from tasks_task t " +
-    		"inner join tasks_task_spectators ts on t.id=ts.task_id " + 
-    		"inner join user_user u on ts.user_id=u.id " + 
-    		"inner join tasks_task_spectator_groups tsg on t.id = tsg.task_id " + 
-    		"inner join user_group ug on tsg.group_id=ug.codename " +
-    		"where u.id=?1 or ug.codename in ?2 or t.responsible_id=?1 or t.responsible_group_id in ?2",
+    		"left join tasks_task_spectators ts on t.id=ts.task_id " +
+    		"left join user_user u on ts.user_id=u.id " +
+    		"left join tasks_task_spectator_groups tsg on t.id = tsg.task_id " +
+    		"left join user_group ug on tsg.group_id=ug.codename " +
+			"left join taskaccess_accessible ta on t.id=ta.task_id " +
+			"left join user_user u1 on ta.user_id=u1.id " +
+			"left join taskaccess_groupaccessible tag on t.id=tag.task_id " +
+			"left join user_group ug1 on tag.group_id=ug1.codename "+
+    		"where u.id=?1 or ug.codename in ?2 or t.responsible_id=?1 or t.responsible_group_id in ?2 " +
+				" or u1.id=?1 or ug1.codename in ?2",
     		countQuery = "select count(distinct (tt.id)) from (select t.* from tasks_task t " +
-    	    		"inner join tasks_task_spectators ts on t.id=ts.task_id " + 
-    	    		"inner join user_user u on ts.user_id=u.id " + 
-    	    		"inner join tasks_task_spectator_groups tsg on t.id = tsg.task_id " + 
-    	    		"inner join user_group ug on tsg.group_id=ug.codename " +
+    	    		"left join tasks_task_spectators ts on t.id=ts.task_id " +
+    	    		"left join user_user u on ts.user_id=u.id " +
+    	    		"left join tasks_task_spectator_groups tsg on t.id = tsg.task_id " +
+    	    		"left join user_group ug on tsg.group_id=ug.codename " +
+					"left join taskaccess_accessible ta on t.id=ta.task_id " +
+					"left join user_user u1 on ta.user_id=u1.id "+
+					"left join taskaccess_groupaccessible tag on t.id=tag.task_id " +
+					"left join user_group ug1 on tag.group_id=ug1.codename "+
     	    		"where u.id=?1  or ug.codename in ?2 or t.responsible_id=?1 or t.responsible_group_id in ?2" +
+					" or u1.id=?1 or ug1.codename in ?2" +
     	    ") tt",
     		nativeQuery = true)
 	Page<Task> findWatchingTasks(Long userid, List<String> groups, Pageable pageable);
     
     @Query(value = "select * from tasks_task tt where tt.id in ( select distinct t.id from tasks_task t " +
-                        "inner join user_user u on t.created_by_id=u.id " +
-                        "inner join markup_markup m on t.description_id = m.id " +
-                        "inner join tasks_comment tc on tc.task_id = t.id "+
-                        "inner join markup_markup mm on tc.content_id = mm.id "+
-						"inner join user_user ur on t.responsible_id = ur.id "+
+                        "left join user_user u on t.created_by_id=u.id " +
+                        "left join markup_markup m on t.description_id = m.id " +
+                        "left join tasks_comment tc on tc.task_id = t.id "+
+                        "left join markup_markup mm on tc.content_id = mm.id "+
+						"left join user_user ur on t.responsible_id = ur.id " +
+						"left join user_group ug on t.responsible_group_id = ug.codename "+
                    "where " +
-                        "UPPER(u.fullname) like %?1% " +
-                        "and UPPER(t.subject) like %?2% " +
-                        "and UPPER(m.text) like %?3% " +
-                        "and UPPER(mm.text) like %?4% " +
-						"and UPPER(ur.fullname) like %?5%) ",
+                        "COALESCE(UPPER(u.fullname),'') like %?1% " +
+                        "and COALESCE(UPPER(t.subject),'') like %?2% " +
+                        "and COALESCE(UPPER(m.text),'') like %?3% " +
+                        "and COALESCE(UPPER(mm.text),'') like %?4% " +
+						"and (COALESCE(UPPER(ur.fullname),'') like %?5% " +
+						"or COALESCE(UPPER(ug.name),'') like %?5%))",
           countQuery = "select count(distinct(t.id)) from tasks_task t " +
-                        "inner join user_user u on t.created_by_id=u.id " +
-                        "inner join markup_markup m on t.description_id = m.id " +
-                        "inner join tasks_comment tc on tc.task_id = t.id "+
-                        "inner join markup_markup mm on tc.content_id = mm.id "+
-					    "inner join user_user ur on t.responsible_id = ur.id "+
+                        "left join user_user u on t.created_by_id=u.id " +
+                        "left join markup_markup m on t.description_id = m.id " +
+                        "left join tasks_comment tc on tc.task_id = t.id "+
+                        "left join markup_markup mm on tc.content_id = mm.id "+
+					    "left join user_user ur on t.responsible_id = ur.id "+
+						"left join user_group ug on t.responsible_group_id = ug.codename "+
                    "where " +
-                        "UPPER(u.fullname) like %?1% " +
-                        "and UPPER(t.subject) like %?2% " +
-                        "and UPPER(m.text) like %?3% " +
-                        "and UPPER(mm.text) like %?4% " +
-				  		"and UPPER(ur.fullname) like %?5% ",
+                        "COALESCE(UPPER(u.fullname),'') like %?1% " +
+                        "and COALESCE(UPPER(t.subject),'') like %?2% " +
+                        "and COALESCE(UPPER(m.text),'') like %?3% " +
+                        "and COALESCE(UPPER(mm.text),'') like %?4%  " +
+				  		"and (COALESCE(UPPER(ur.fullname),'') like %?5% " +
+				  		"or COALESCE(UPPER(ug.name),'') like %?5%)",
           nativeQuery = true)
     Page<Task> findTasksByVarParam(String author, String subject, String description, String comment, String responsible, Pageable pageable);
 
