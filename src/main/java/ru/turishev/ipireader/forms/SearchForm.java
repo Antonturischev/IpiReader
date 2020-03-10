cd793ce1f3b1df8046d09f0812e658f8de82d4bb
antonturischev <antonturischev@gmail.com>
2020-03-10 20:23:29 +0700

    10032020

--- src/main/java/ru/turishev/ipireader/forms/SearchForm.java
+++ src/main/java/ru/turishev/ipireader/forms/SearchForm.java
@@ -20,7 +20,6 @@ public class SearchForm {
     private String responsible;
     private String datecreatedd;
     private String datecreatedu;
-
     public List<SearchParameter> toList() {
         List<SearchParameter> list = new ArrayList<>();
         list.add(new SearchParameter(1,"author","Автор",(author!=null)?author:"",(author!=null)?"disabled":"", "text"));
@@ -28,10 +27,8 @@ public class SearchForm {
         list.add(new SearchParameter(3,"description","Описание",(description!=null)?description:"",(description!=null)?"disabled":"", "text"));
         list.add(new SearchParameter(4,"comment","Комментарий",(comment!=null)?comment:"",(comment!=null)?"disabled":"", "text"));
         list.add(new SearchParameter(5,"responsible","Ответственный",(responsible!=null)?responsible:"",(responsible!=null)?"disabled":"", "text"));
-        list.add(new SearchParameter(6,"datecreatedd","Дата создания больше",(datecreatedd!=null)?datecreatedd:"1990-01-01",(datecreatedd!=null)?"disabled":"", "date"));
-        list.add(new SearchParameter(7,"datecreatedu","Дата создания меньше",(datecreatedu!=null)?datecreatedu:"2030-01-01",(datecreatedu!=null)?"disabled":"", "date"));
+        list.add(new SearchParameter(6,"datecreatedd","Дата создания больше",((datecreatedd!=null)&&(!datecreatedd.equals("")))?datecreatedd:"",(datecreatedd!=null)?"disabled":"", "date"));
+        list.add(new SearchParameter(7,"datecreatedu","Дата создания меньше",((datecreatedu!=null)&&(!datecreatedu.equals("")))?datecreatedu:"",(datecreatedu!=null)?"disabled":"", "date"));
         return list;
     }
-
-
 }
--- src/main/java/ru/turishev/ipireader/services/SearchService.java
+++ src/main/java/ru/turishev/ipireader/services/SearchService.java
@@ -17,19 +17,17 @@ import java.util.List;
 public class SearchService {
     @Autowired
     private TasksRepository tasksRepository;
+
     public Page<TasksDto> getTasksBySearchParameters(List<SearchParameter> searchParameters, Pageable pageable) {
-    	String datecreatedd = searchParameters.get(5).getSelectedValue().trim();
-    	String datecreatedu = searchParameters.get(6).getSelectedValue().trim();
-    	datecreatedd = (datecreatedd.equals(""))?"1990-01-01 00:00:00":datecreatedd+" 00:00:00";
-    	datecreatedu = (datecreatedu.equals(""))?"2030-01-01 00:00:00":datecreatedu+" 23:59:59";
-    	
+        String datecreatedd=(searchParameters.get(5).getSelectedValue().equals(""))?"1990-01-01 00:00:00":searchParameters.get(5).getSelectedValue()+" 00:00:00";
+        String datecreateud=(searchParameters.get(6).getSelectedValue().equals(""))?"2030-01-01 00:00:00":searchParameters.get(6).getSelectedValue()+" 23:59:59";
         Page<Task> tasks = tasksRepository.findTasksByVarParam(searchParameters.get(0).getSelectedValue().trim().toUpperCase(),
                                                                searchParameters.get(1).getSelectedValue().trim().toUpperCase(),
                                                                searchParameters.get(2).getSelectedValue().trim().toUpperCase(),
                                                                searchParameters.get(3).getSelectedValue().trim().toUpperCase(),
                                                                searchParameters.get(4).getSelectedValue().trim().toUpperCase(),
-                                                               Timestamp.valueOf(datecreatedd),
-                                                               Timestamp.valueOf(datecreatedu),
+                                                               Timestamp.valueOf(datecreatedd.trim()),
+                                                               Timestamp.valueOf(datecreateud.trim()),
                 pageable);
         Page<TasksDto> tasksDto = tasks.map(Utils::convertToTasksDto);
         return tasksDto;
